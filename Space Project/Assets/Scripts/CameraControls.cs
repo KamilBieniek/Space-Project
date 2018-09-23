7acd4b19d91d430d439f73e573ba456d781398cc
Kabi <kamilbieniek97@gmail.com>
2018-09-23 18:33:07 +0200

    Uploading first version

---
+++ Space Project/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraControls : MonoBehaviour {
+
+	public float globalSpeed;
+	public Text information;
+	public GameObject canvas;
+
+	private Quaternion initialRotation;
+	private Vector3 initialPosition;
+
+
+	void Start () 
+	{
+		globalSpeed = 1f;
+		initialPosition = transform.position;
+		initialRotation = transform.rotation;
+	}
+	
+
+	void Update () 
+	{
+		//Changing UI text.
+		if (globalSpeed == 1f) 
+		{
+			information.text = "1 second = " + globalSpeed + " day";
+		} 
+		else 
+		{
+			information.text = "1 second = " + globalSpeed + " days";
+		}
+
+		CameraMovement ();
+		CameraRestart ();
+		HidingUI ();
+	}
+
+	void HidingUI()
+	{
+		if (Input.GetKeyDown (KeyCode.U))
+		{
+			canvas.SetActive(false);
+		}
+		if (Input.GetKeyUp (KeyCode.U))
+		{
+			canvas.SetActive(true);
+		}
+	}
+
+	void CameraRestart()
+	{
+		if (Input.GetKey (KeyCode.R)) 
+		{
+			transform.position = initialPosition;
+			transform.rotation = initialRotation;
+		}
+	}
+
+	void CameraMovement()
+	{
+		//Moving the camera.
+		if (Input.GetKey (KeyCode.W))
+		{
+			transform.position = transform.position + transform.forward * 45 * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			transform.position = transform.position - transform.forward * 45 * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.position = transform.position - transform.right * 45 * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D))
+		{
+			transform.position = transform.position + transform.right * 45 * Time.deltaTime;
+		}
+
+		//Rotating the camera.
+		if (Input.GetKey (KeyCode.Keypad6))
+		{
+			transform.Rotate (Vector3.up, relativeTo:Space.World);
+		}
+		if (Input.GetKey (KeyCode.Keypad4))
+		{
+			transform.Rotate (Vector3.down, relativeTo:Space.World);
+		}
+		if (Input.GetKey (KeyCode.Keypad8))
+		{
+			transform.Rotate (Vector3.left, relativeTo:Space.Self);
+		}
+		if (Input.GetKey (KeyCode.Keypad2))
+		{
+			transform.Rotate (Vector3.right, relativeTo:Space.Self);
+		}
+	}
+
+	//Just to have access from UI's slider.
+	public void speedChange (float setSpeed)
+	{
+		globalSpeed = setSpeed;
+	}
+		
+}
---
+++ Space Project/Assets/Scripts/PlanetMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour {
+
+	public float earthDays;
+	public float daysToGoRoundSun;
+	public float speed;
+	public Vector3 ellipseCenter;
+	public Canvas canvas;
+	public float angle;
+	public float ellipseWidth;
+	public float ellipseHeight;
+
+	private CameraControls globalCamera;
+	private TrailRenderer tr;
+	private bool isSun;
+
+	void Start () 
+	{
+		globalCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraControls> ();
+
+		if (this.name == "Sun Holder") 
+		{
+			isSun = true;
+		} 
+		else 
+		{
+			tr = GetComponent<TrailRenderer> ();
+		}
+	}
+
+	void Update () {
+
+		speed = globalCamera.globalSpeed;
+
+		SelfRotation ();
+
+		if (!isSun) 
+		{
+			PlanetNamesMovement ();
+			AroundSunRotation ();
+			TrailControl ();
+		}
+	}
+		
+	void TrailControl()
+	{
+		tr.time = (0.95f * daysToGoRoundSun) / speed;
+
+		//Turning off the trail with the rest of UI.
+		if (Input.GetKey (KeyCode.U)) {
+			tr.emitting = false;
+			tr.time = 0f;
+		} 
+		else 
+		{
+			tr.emitting = true;
+		}
+	}
+
+	void PlanetNamesMovement()
+	{
+		//Planets name's follow their planets.
+		canvas.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+
+		//Making names over planets always look at the camera direction, unless it's too close.
+		if (Vector3.Distance (globalCamera.transform.position, canvas.transform.position) > 50) 
+		{
+			canvas.transform.LookAt (new Vector3(globalCamera.transform.position.x, canvas.transform.position.y, globalCamera.transform.position.z), worldUp: Vector3.up);
+		}
+	}
+
+	void SelfRotation()
+	{
+			transform.Rotate(Vector3.up, -4 * Time.deltaTime * speed * 60 / earthDays);
+	}
+
+	void AroundSunRotation()
+	{
+		transform.position = new Vector3 (ellipseCenter.x + ellipseWidth * Mathf.Cos (angle*(Mathf.PI/180)), transform.position.y, ellipseCenter.z + ellipseHeight * Mathf.Sin (angle*(Mathf.PI/180)));
+		angle = angle + 360 * Time.deltaTime * speed / daysToGoRoundSun;
+
+		//Using angles periodicity to prevent it's value from going to infnity.
+		if (angle > 360) 
+		{
+			angle = angle - 360;
+		}
+	}
+}
